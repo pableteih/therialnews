e8531b5e39523d46d4dfd78a57e78b0a52845763
Pablo Igor <pablo.igor.hess@gmail.com>
2023-07-10 19:10:03 -0400

    se traen las vistas y controladores de user+ configuracion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :publications
   get 'homes/index'
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions: "users/sessions",
+  registrations: "users/registrations"}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
