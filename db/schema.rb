44f53118afd0931d32a86669053ad9bb0afb3f23
Pablo Igor <pablo.igor.hess@gmail.com>
2023-07-18 14:32:23 -0400

    configuracion de roles a 1

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_205642) do
     t.string "phone"
     t.string "name"
     t.integer "age"
-    t.integer "role", default: 0
+    t.integer "role", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
