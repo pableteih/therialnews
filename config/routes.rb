2b4ccacd372eb476cf5a2e2cf8fa7c67ef8222f4
Pablo Igor <pablo.igor.hess@gmail.com>
2023-07-17 23:39:17 -0400

    lastcommit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "homes#index"
+  root "publications#index"
   get "/publications", to: "publications#index",  as: "user_root"
 end
